e4c4a159e723ea83a49d46bdcdccc56497937944
CharanTeja <rcharanteja2006@gmail.com>
2025-08-10 10:58:28 +0530

    modded App.jsx for git actions

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ function App() {
 
   return (
     <div style={{color:"blue", display:"flex", justifyContent:"center", alignItems:"center", fontFamily:"monospace", height:"100vh", fontSize:"2.5rem", flexDirection:"column"}}> 
-        <h1>This is a practice <u>React app</u> for deployment on Github pages</h1>
+        <h2>This is a practice <u>React app</u> for deployment on Github pages</h2>
         <h2 style={{color:"#00ff00"}}>Deployment Status : SUCCESS</h2>
+        <h2>Now this process of <b style={{fontWeight:"800"}}>Deployment</b> is <u>Automated</u> using <span style={{color:"red"}}>Git Actions YAML Script</span></h2>
     </div>
   )
 }
